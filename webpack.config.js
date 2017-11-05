3dbe8681844254f07935e897f18b465dbce5fd32
Nicolás López Zerpa <nico89abc@gmail.com>
2017-11-05 18:20:11 -0300

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     context: __dirname + "/src/",
     output: {
-        path: path.resolve(__dirname, "public"),
+        path: path.resolve(__dirname, "public/dist"),
         filename: "bundle.js"
     },
 
